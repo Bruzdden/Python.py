e56a464582206ed2b2806c60d604f54063ee1ceb
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2021-11-26 09:14:52 +0100

    Create cislo.py

---
+++ cislo.py
@@ -0,0 +1,5 @@
+cislo = int(input("Číslo: "))
+if (cislo % 2 == 0 or cislo % 3 == 0 or cislo % 5 == 0):
+    print("\nČíslo je násobkem 2 nebo 3 nebo 5")
+else:
+    print("\nČíslo není násobkem 2 nebo 3 nebo 5")
