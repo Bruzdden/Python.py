61b3585b86ea4263ba6ec282efd0b3db78fcce11
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2021-12-02 11:13:29 +0100

    Create roky.py

---
+++ roky.py
@@ -0,0 +1,27 @@
+rok = int(input("rok: "))
+print(rok)
+if rok % 400 == 0:
+    print("\nrok je přestupný")
+elif rok % 100 == 0:
+    print("\nnení přestupný rok")   
+elif rok % 4 ==0:
+    print("\nrok je přestupný")
+else:
+    print("\nnení přestupný rok")    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #27
