ee00ab7a643a1912bb972e50cd46c971f65f77ee
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-04-05 16:56:00 +0200

    Update Adressing.py

--- Adressing.py
+++ Adressing.py
@@ -5,3 +5,6 @@ IPadresa = ipaddress.IPv4Network(sit)
 print("Maska adresy:", IPadresa.netmask)
 print("Network adresy:", IPadresa.network_address)
 print("Broadcast adresy:", IPadresa.broadcast_address)
+adres = IPadresa.num_addresses
+print("Počet adres:", adres)
+print("Počet hostů:", adres-2
