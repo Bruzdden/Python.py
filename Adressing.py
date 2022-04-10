c835aa9274493c3ad1c4b4d47a60d25ddaf1bdf7
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-04-10 20:14:56 +0200

    Update Adressing.py

--- Adressing.py
+++ Adressing.py
@@ -1,10 +1,28 @@
 import ipaddress
+from appJar import gui
 
 sit = input("napis sit:")
 IPadresa = ipaddress.IPv4Network(sit)
-print("Maska adresy:", IPadresa.netmask)
-print("Network adresy:", IPadresa.network_address)
-print("Broadcast adresy:", IPadresa.broadcast_address)
+maska = print("Maska adresy:", IPadresa.netmask)
+network= print("Network adresy:", IPadresa.network_address)
+broadcast=print("Broadcast adresy:", IPadresa.broadcast_address)
 adres = IPadresa.num_addresses
-print("Počet adres:", adres)
-print("Počet hostů:", adres-2
+adresy =print("Počet adres:", adres)
+hosti = print("Počet hostů:", adres-2)
+ 
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+app = gui()
+app.setSticky("news")
+fileMenu = ["Quit"]
+app.addMenuList("Adresy", fileMenu, onMenuItemSelect)
+tabulka = [["",""],
+  ["Maska adresy", IPadresa.netmask],
+             ["Network adresy", IPadresa.network_address],
+             ["Broadcast adresy", IPadresa.broadcast_address],
+             ["Počet adres", adres],
+           ["Počet hostů", adres-2]]
+app.addGrid("grid", tabulka)
+ 
+app.go()
