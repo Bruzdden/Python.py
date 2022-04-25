5846615c4f29737b8b9db727a4d5de66b9f2702f
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-04-25 13:09:47 +0200

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -16,25 +16,30 @@ class Car(Vehicle):
 class Motorbike(Vehicle):
     def __init__(self,zvuk, kolo):
         super().__init__(zvuk, kolo)
-    def stoji(self, znacka):
+    def stoji(self, znacka, vlastnik):
         self.znacka = znacka
-        print(f"Motorka stoji a ma znacku {self.znacka}")
+        self.vlastnik = vlastnik
+        print(f"Motorka stoji a ma znacku {self.znacka} a vlastní ji {self.vlastnik}")
     def __str__(self):
         return f"Vozidlo je {type(self).__name__} a ma zvuk {self.zvuk} a ma {self.kolo} kola"
 class Truck(Vehicle):
-    def __init__(self,zvuk, kolo):
+    def __init__(self,zvuk, kolo, rok):
         super().__init__(zvuk, kolo)
+        self.rok = rok
     def nemaparu(self, znacka):
         self.znacka = znacka
         print(f"Truck nemaparu a ma znacku {self.znacka}")
     def __str__(self):
-        return f"Vozidlo je {type(self).__name__} a ma zvuk {self.zvuk} a ma {self.kolo} kola"
+        return f"Vozidlo je {type(self).__name__} a ma zvuk {self.zvuk} a ma {self.kolo} kola, je z roku {self.rok}"
 
-vehicle = Vehicle("vrrr", 0)
-auto = Car("vrrr",4)
-auto.startuje("subaru")
-motorka = Motorbike("vr", 2)
-motorka.stoji("Honda")
-truck = Truck("vrrrrrrr",4)
+zvuk = str(input("napis zvuk auta:"))
+zvukm = str(input("napis zvuk motorky:"))
+zvukt = str(input("napis zvuk trucku:"))
+vehicle = Vehicle("vrrr", "nekolik")
+auto = Car(zvuk,4)
+auto.startuje("Subaru")
+motorka = Motorbike(zvukm, 2)
+motorka.stoji("Honda","Patrik")
+truck = Truck(zvukt,4,2001)
 truck.nemaparu("Jeep")
 print(vehicle); print(auto); print(motorka); print(truck)
