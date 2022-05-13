799985899c577aa4bb50881ce543f5d7a7312bb9
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-05-13 11:08:14 +0200

    Update sql.py

--- sql.py
+++ sql.py
@@ -2,12 +2,19 @@ import mysql.connector as db
 from mysql.connector import Error
 
 
-connection = db.connect(host='localhost', database='comics', user='root', password='root')
+connection = db.connect(
+    host='localhost', 
+    database='comics', 
+    user='root', 
+    password='root')
    
 db_Info = connection.get_server_info()
 print("Connected to MySQL Server version ", db_Info)
 insert = connection.cursor()
 q = "INSERT INTO charact (id, name, weapon, studio_id) VALUES (NULL, %s, %s, %s)"
 s = ("Petr","Kozak", 1)
-insert.execute(q, s)
+n = str(input("vlož do tabulky jmeno: "))
+w = str(input("zbran:"))
+id = int(input("id:"))
+insert.execute(q, (n,w,id))
 connection.commit()
